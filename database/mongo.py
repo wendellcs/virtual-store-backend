8b16685384cbe1f32b51abde8745bce36712aaba
Wendell <wendellcs131@gmail.com>
2026-01-05 22:59:46 -0300

    Update mongo.py and requirements

--- database/mongo.py
+++ database/mongo.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import ssl
 import os
 
 load_dotenv()
@@ -8,7 +9,12 @@ MONGO_URL = os.getenv('MONGO_URL')
 if not MONGO_URL:
     raise RuntimeError('MONGO_URL não encontrada no .env')
 
-client = MongoClient(MONGO_URL)
+client = MongoClient(
+    MONGO_URL,     
+    tls=True,
+    tlsAllowInvalidCertificates=False,
+    ssl_cert_reqs=ssl.CERT_REQUIRED
+)
 
 db = client['compra-facil']
 collection = db['produtos']
